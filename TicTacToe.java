b990e50c5a2ba8d6d3e8c5370cdc4153280eb94e
Xander White <xaw8720@g.rit.edu>
2025-02-22 16:03:22 -0500

    Add font size scaling

--- TicTacToe.java
+++ TicTacToe.java
@@ -7,23 +7,25 @@ import java.awt.event.ActionEvent;
 
 public class TicTacToe {
     public static void main(String[] args) {
-        int grid_width = 3;
-        int grid_height = 3;
+        int display_size = 900;
+
+        // Must be less than 9
+        int grid_size = 3;
 
         final boolean[] player1 = {true};
 
         // Create the frame
         JFrame frame = new JFrame("Tic Tac Toe");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900, 900);
+        frame.setSize(display_size, display_size);
 
         // Create a grid layout
-        frame.setLayout(new GridLayout(grid_width, grid_height));
+        frame.setLayout(new GridLayout(grid_size, grid_size));
 
         // Add buttons to the frame
-        for (int i = 0; i < (grid_width * grid_height); i++) {
+        for (int i = 0; i < (grid_size * grid_size); i++) {
             JButton button = new JButton();
-            button.setFont(new Font("Arial", Font.PLAIN, 40));
+            button.setFont(new Font("Arial", Font.PLAIN, display_size / grid_size));
             button.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     if (!button.getText().equals("X") && !button.getText().equals("O")) {
